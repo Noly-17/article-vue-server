4202c1a8c892fb59937e3a778a1ed15ac343c29d
Noly-17 <devnoly17@gmail.com>
2024-10-25 18:17:40 +0800

    vercel tracked config

--- src/index.js
+++ src/index.js
@@ -25,3 +25,5 @@ app.use('/api/v1', [authRoutes, articleRoutes]);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default app;
